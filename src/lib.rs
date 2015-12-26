ffb93021199a63e9f1eddf7df70d9ff786a67faf
Lionel Flandrin <lionel.flandrin@gmail.com>
2015-12-26 21:33:25 +0100

    Library stub

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+
+/// Version of the rustation library set in Cargo.toml
+pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+/// Like VERSION but as a `\0`-terminated C string. Useful when you
+/// need a static string in C bindings.
+pub const VERSION_CSTR: &'static str = concat!(env!("CARGO_PKG_VERSION"), '\0');
